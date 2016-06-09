d7c2f9dbe3d87fabef0547aea7e006e1ae65b940
Kostas Makris <Konstantinos.Makris@marks-and-spencer.com>
2016-06-09 15:42:36 +0100

    add project

---
+++ src/test/java/com/tests/AddToBasketTest.java
@@ -0,0 +1,14 @@
+package com.tests;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/com/tests/features/add_to_basket.feature", format = {
+"pretty", "json:target/cucumber-reports/add_to_basket.feature"}, tags = {"@test"})
+public class AddToBasketTest {
+	
+
+}
---
+++ src/test/java/com/tests/features/add_to_basket.feature
@@ -0,0 +1,10 @@
+@mytest @test
+Feature: As a Customer
+I want to be able to add items to my basket
+So that I can purchase them
+
+  Scenario: add basket
+ 
+  When I add item
+ 
+
